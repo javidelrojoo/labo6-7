2eb33fed55547a6219b5893891581973d3fa53a2
Javi Tau <javitau459@gmail.com>
2023-03-27 23:59:43 -0300

    Agrego una funcion para hacer graficos de bode

--- TonghuiTH283X.py
+++ TonghuiTH283X.py
@@ -1,5 +1,6 @@
 import pyvisa
 import numpy as np
+import matplotlib.pyplot as plt
 
 class TH283X:
     def __init__(self, name):
@@ -76,11 +77,29 @@ class TH283X:
     
     def make_EI(self, frecs, func):
         n = len(frecs)
-        Rs = np.zeros(n)
-        Xs = np.zeros(n)
-        fs = np.zeros(n)
+        Z = np.zeros(n)
+        phase = np.zeros(n)
+        f = np.zeros(n)
         for i, frec in enumerate(frecs):
             self.set_freq(frec)
-            Rs[i], Xs[i] = self.measure(func)
-            fs[i] = self.get_freq()
-        return fs, Rs, Xs
+            Z[i], phase[i] = self.measure(func)
+            f[i] = self.get_freq()
+        self.make_bode_plot(f, Z, phase)
+        return f, Z, phase
+    
+    def make_bode_plot(self, f, Z, phase):
+        fig, axs = plt.subplots(2, 1, sharex=True)
+
+        axs[0].plot(f, 20*np.log10(Z), 'C0')
+        axs[0].set_ylabel('Ganancia [dB]')
+        axs[0].set_xscale('log')
+        axs[0].grid()
+
+        axs[1].plot(f, np.abs(phase), 'C1')
+        axs[1].set_ylabel('Fase [°]')
+        axs[1].set_xlabel('Frecuencia [Hz]')
+        axs[1].grid()
+
+        plt.tight_layout()
+        plt.show()
+        return
