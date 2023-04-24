cf692fb3abff1a59b8fffa156f1dc5bcdd768373
javitau <javitau459@gmail.com>
2023-04-24 19:13:22 -0300

    Agrego mediciones del 24/4

--- K2612B/initializate.py
+++ K2612B/initializate.py
@@ -36,43 +36,54 @@ functions.startSMU(smu)
 functions.loadScripts(smu)
 
 # Para V= 0.4, rangeI= 5e-4
-N = 25
+# Para V = 0.1, rangeI = 5e-8
+N = 20
 Vpos = 5
-Vneg = 5
+Vneg = 3
 stepPos = Vpos/N
 stepNeg = Vneg/N
 rev = 0
 hslV = 0.3
-hslF = 1
-cycles = 2
+hslF = 0
+cycles = 1
 T = 2.5e-1
-pw = 5e-2
-limitI = 5e-2
-rangeI = limitI/5
+pw = 2.5e-2
+limitI = 5e-4
+rangeI = 1e-8
 limitV = 0.5
 rangeV = 20
-nplc = 0.001
+nplc = 0.1
 t, volt, curr = runner.iv(smu,Vpos,Vneg,stepPos,stepNeg,rev,hslV,hslF,cycles,T,pw,limitI,rangeI,limitV,rangeV,nplc)
-save_csv(t, volt, curr, filename='Al-Au(E2-F3)-ida-vuelta-bias4', root='results/', delimiter=',', header=f'Tiempo [s], Voltage [V], Corriente [A]\n Vpos={Vpos}, Vneg={Vneg}, stepPos={stepPos}, stepNeg={stepNeg}, rev={rev}, hslV={hslV}, hslF={hslF}, cycles={cycles}, T={T}, pw={pw}, limitI={limitI}, rangeI={rangeI}, limitV={limitV}, rangeV={rangeV}, nlpc={nplc}')
+save_csv(t, volt, curr, filename='Al-Au(C4-D5)-ida-vuelta-post1.5V-5-3V', root='results/', delimiter=',', header=f'Tiempo [s], Voltage [V], Corriente [A]\n Vpos={Vpos}, Vneg={Vneg}, stepPos={stepPos}, stepNeg={stepNeg}, rev={rev}, hslV={hslV}, hslF={hslF}, cycles={cycles}, T={T}, pw={pw}, limitI={limitI}, rangeI={rangeI}, limitV={limitV}, rangeV={rangeV}, nlpc={nplc}')
 
 
-plt.plot(t[::2], abs(curr)[::2], '-o')
+plt.plot(volt, abs(curr), '-o')
 plt.grid()
-plt.figure()
-plt.plot(t[1::2], volt[1::2])
-plt.grid()
-plt.figure()
-plt.plot(volt[1::2], abs(curr)[1::2], '-o')
-plt.grid()
+# plt.figure()
+# plt.plot(t[1::2], volt[1::2])
+# plt.grid()
+# plt.figure()
+# plt.plot(volt[1::2], abs(curr)[1::2], '-o')
+# plt.grid()
 plt.show()
 
 for filename in os.listdir('./results/'):
-    if filename.startswith('Al-Au(E2-F3)-ida-vuelta-bias'):
-        t1, volt1, curr1 = np.loadtxt('results/' + filename, delimiter=',', unpack=True, skiprows=2)
-        plt.plot(volt1[1::2], abs(curr1)[1::2], '-o')
+    if filename.startswith('Al-Au(E3-F2)-ida-vuelta'):
+        if filename.startswith('Al-Au(E2-F3)-ida-vuelta-bias'):
+            pass
+        else:
+            t1, volt1, curr1 = np.loadtxt('results/' + filename, delimiter=',', unpack=True, skiprows=2)
+            if volt1.max() < 0.45:
+                pass
+            else:
+                plt.plot(volt1, abs(curr1)*1000, '-o') #NOBIAS
+            # plt.plot(volt1[1::2], abs(curr1)[1::2]*1000, '-o') #BIAS
         # plt.plot(t1[::2], abs(curr1)[::2], '-o')
+plt.xlabel('Tensión [V]')
+plt.ylabel('|Corriente| [mA]')
 plt.grid()
 plt.show()
+plt.savefig('../graficos/'+'Au-Au(E3-F2)-ida-vuelta'+'.png')
 
 # Esto era para barrido en una sola direccion
 # minV = 0
