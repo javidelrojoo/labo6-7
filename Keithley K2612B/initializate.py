49a3cc746f5f856c700a636a591fe81a96e4fa48
javidelrojoo <javitau459@gmail.com>
2023-05-03 19:23:46 -0300

    Mediciones con el smu del 3/5

--- Keithley K2612B/initializate.py
+++ Keithley K2612B/initializate.py
@@ -39,48 +39,95 @@ functions.loadScripts(smu)
 
 # Para V= 0.4, rangeI= 5e-4
 # Para V = 0.1, rangeI = 5e-8
-N = 25
-Vpos = 0.1
-Vneg = 0.1
+N = 75
+Vpos = 5
+Vneg = 5
 if Vpos > 0.1 or Vneg > 0.1:
     input('Este voltaje probablemente escriba el dispositivo, presione ENTER para continuar')
 stepPos = Vpos/N
 stepNeg = Vneg/N
 rev = 0
-hslV = 0.3
-hslF = 0
-cycles = 2
+hslV = 0.4
+hslF = 1
+cycles = 5
 T = 2.5e-1
 pw = 2.5e-2
 limitI = 5e-4
 rangeI = 1e-8
 limitV = 0.5
 rangeV = 20
-nplc = 0.1
+nplc = 0.01
 t, volt, curr = runner.iv(smu,Vpos,Vneg,stepPos,stepNeg,rev,hslV,hslF,cycles,T,pw,limitI,rangeI,limitV,rangeV,nplc)
-save_csv(t, volt, curr, filename='Al-Au(B5-B6)-post-2-7V', root='results/', delimiter=',', header=f'Tiempo [s], Voltage [V], Corriente [A]\n Vpos={Vpos}, Vneg={Vneg}, stepPos={stepPos}, stepNeg={stepNeg}, rev={rev}, hslV={hslV}, hslF={hslF}, cycles={cycles}, T={T}, pw={pw}, limitI={limitI}, rangeI={rangeI}, limitV={limitV}, rangeV={rangeV}, nlpc={nplc}')
+save_csv(t, volt, curr, filename='Al-Au(E7-E8)-bias5-5V5ciclos', root='results/', delimiter=',', header=f'Tiempo [s], Voltage [V], Corriente [A]\n Vpos={Vpos}, Vneg={Vneg}, stepPos={stepPos}, stepNeg={stepNeg}, rev={rev}, hslV={hslV}, hslF={hslF}, cycles={cycles}, T={T}, pw={pw}, limitI={limitI}, rangeI={rangeI}, limitV={limitV}, rangeV={rangeV}, nlpc={nplc}')
 
 
 # plt.plot(volt, abs(curr), '-o')
 # plt.grid()
-fig, ax = plt.subplots()
 
-points = np.array([volt, abs(curr)]).T.reshape(-1, 1, 2)
-segments = np.concatenate([points[:-1], points[1:]], axis=1)
-# Create a continuous norm to map from data points to colors
-norm = plt.Normalize(t.min(), t.max())
-lc = LineCollection(segments, cmap='viridis', norm=norm) #, linestyles='dashed'
-# Set the values used for colormapping
-lc.set_array(t)
-lc.set_linewidth(2)
-line = ax.add_collection(lc)
-# fig.colorbar(line, ax=ax)
-ax.set_xlim(volt.min(), volt.max())
-ax.set_ylim(abs(curr).min(), abs(curr).max())
-ax.scatter(volt, abs(curr))
-ax.grid(True)
-# ax.set_yscale('log')
-
+volt, curr = plot('d')
+volt, curr = plot('bd')
+volt_d, r = plot('b')
+def plot(mode):
+    fig, ax = plt.subplots()
+    mode = mode
+    if mode=='d':    
+        points = np.array([volt, abs(curr)]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+        # Create a continuous norm to map from data points to colors
+        norm = plt.Normalize(t.min(), t.max())
+        lc = LineCollection(segments, cmap='viridis', norm=norm) #, linestyles='dashed'
+        # Set the values used for colormapping
+        lc.set_array(t)
+        lc.set_linewidth(2)
+        line = ax.add_collection(lc)
+        # fig.colorbar(line, ax=ax)
+        ax.set_xlim(volt.min(), volt.max())
+        ax.set_ylim(abs(curr).min(), abs(curr).max())
+        ax.scatter(volt, abs(curr), c=t)
+        ax.grid(True)
+        # ax.set_yscale('log')
+        return volt, abs(curr)
+    elif mode=='bd':
+        volt_d = volt[1::2]
+        curr_d = curr[1::2]
+        points = np.array([volt_d, abs(curr_d)]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+        # Create a continuous norm to map from data points to colors
+        norm = plt.Normalize(t.min(), t.max())
+        lc = LineCollection(segments, cmap='viridis', norm=norm) #, linestyles='dashed'
+        # Set the values used for colormapping
+        lc.set_array(t)
+        lc.set_linewidth(2)
+        line = ax.add_collection(lc)
+        # fig.colorbar(line, ax=ax)
+        ax.set_xlim(volt_d.min(), volt_d.max())
+        ax.set_ylim(abs(curr_d).min(), abs(curr_d).max())
+        ax.scatter(volt_d, abs(curr_d), c=t[1::2])
+        ax.grid(True)
+        # ax.set_yscale('log')
+        return volt, abs(curr)
+    elif mode=='b':
+        volt_b = volt[::2]
+        volt_d = volt[1::2]
+        curr_d = curr[::2]
+        r = volt_b/curr_d
+        points = np.array([volt_d, r]).T.reshape(-1, 1, 2)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+        # Create a continuous norm to map from data points to colors
+        norm = plt.Normalize(t.min(), t.max())
+        lc = LineCollection(segments, cmap='viridis', norm=norm) #, linestyles='dashed'
+        # Set the values used for colormapping
+        lc.set_array(t)
+        lc.set_linewidth(2)
+        line = ax.add_collection(lc)
+        # fig.colorbar(line, ax=ax)
+        ax.set_xlim(volt_d.min(), volt_d.max())
+        ax.set_ylim(r.min(), r.max())
+        ax.scatter(volt_d, r, c=t[::2])
+        ax.grid(True)
+        # ax.set_yscale('log')
+        return volt_d, r
+    
 for filename in os.listdir('./results/'):
     if filename.startswith('Al-Au(E3-F2)-ida-vuelta'):
         if filename.startswith('Al-Au(E2-F3)-ida-vuelta-bias'):
