d51f37d4572bd4103131c416232bc007fda5eff2
Javi Tau <javitau459@gmail.com>
2023-05-16 17:18:12 -0300

    Agrego codigo para mandar notificaciones a telegram

---
+++ send_notification.py
@@ -0,0 +1,30 @@
+import requests
+
+def mensaje_tel(api_token:str, chat_id: str, mensaje: str = 'Termino de correr el script'):
+    '''
+    Manda un mensaje de telegram al chat que tenes con el bot 'python_scripts'.
+    
+    El API token se saca del chat con BotFather. Para obtener el ID del chat us
+    ar el siguiente código. Si no funciona borrar el chat con el bot, inicializ
+    ar de nuevo y probar nuevamente.
+
+    >>url = f'https://api.telegram.org/bot{token}/getUpdates'
+    >>print(requests.get(url).json())
+    
+    INPUT:
+    api_token: str: token del bot creado.
+    chat_id: str: id del chat al que queres mandar el mensaje. 
+    mensaje: str: mensaje para mandar al bot.
+    '''
+    api_url = f'https://api.telegram.org/bot{api_token}/sendMessage'
+    try:
+        response = requests.post(api_url, json={'chat_id': chat_id, 'text': mensaje})
+        print(response.text)
+    except Exception as e:
+        print(e)
+
+mensaje_tel(
+api_token = '6228563199:AAFh4PtD34w0dmV_hFlQC7Vqg3ScI600Djs',
+chat_id = '-1001926663084',
+mensaje = 'Che bostero no me rompas las pelotas'
+)
