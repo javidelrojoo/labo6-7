56fcd096720777b9c086276132a4ed2995f58f0d
Tincho <jerikho175@hotmail.com>
2023-06-11 22:21:26 -0300

    ahora pone fotos

--- send_notification.py
+++ send_notification.py
@@ -23,8 +23,14 @@ def mensaje_tel(api_token:str, chat_id: str, mensaje: str = 'Termino de correr e
     except Exception as e:
         print(e)
 
-mensaje_tel(
-api_token = '6228563199:AAFh4PtD34w0dmV_hFlQC7Vqg3ScI600Djs',
-chat_id = '-1001926663084',
-mensaje = 'Che bostero no me rompas las pelotas'
-)
+def foto_tel(api_token:str, chat_id: str, file_opened: str):
+    params = {'chat_id': chat_id}
+    files = {'photo': file_opened}
+    response = requests.post(f'https://api.telegram.org/bot{api_token}/sendPhoto', params, files=files)
+    return response.text
+
+# mensaje_tel(
+# api_token = '6228563199:AAFh4PtD34w0dmV_hFlQC7Vqg3ScI600Djs',
+# chat_id = '-1001926663084',
+# mensaje = 'Che bostero no me rompas las pelotas'
+# )
