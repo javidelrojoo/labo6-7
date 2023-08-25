439f601999deb84b030d14c1b767700b6dd0698e
javidelrojoo <73077973+javidelrojoo@users.noreply.github.com>
2023-08-25 13:42:34 -0300

    Actualizo nuevas funciones para el keithley

--- Keithley_K2612B/keithleyK2612B.py
+++ Keithley_K2612B/keithleyK2612B.py
@@ -11,7 +11,49 @@ class K2612B:
     
     def __del__(self):
         self._smu.close()
-
+    
+    def hsl(self, Vmax, Vmin, hslV=.4, pw, Npos, Nneg):
+        self._smu.write('smub.source.func = smub.OUTPUT_DCVOLTS')  # Configurar el modo de generación de voltaje a voltaje DC
+        self._smu.write('smub.measure.autorangei = smub.AUTORANGE_ON')
+        self._smu.write('smub.measure.autorangev = smub.AUTORANGE_ON')
+        
+        volt_meas = np.concatenate((np.linspace(0, Vmax, Npos//2, endpoint=False), np.linspace(Vmax, 0, Npos//2, endpoint=False), np.linspace(0, Vmin, Nneg//2, endpoint=False), np.linspace(Vmin, 0, Nneg//2, endpoint=False)))
+        
+        start_time = time.time()
+        
+        t_din = []
+        volt_din = []
+        curr_din = []
+        
+        t_rem = []
+        volt_rem = []
+        curr_rem = []
+        
+        self._smu.write('smub.source.levelv = 0')
+        self._smu.write('smub.source.output = smub.OUTPUT_ON')
+        
+        for i in volt_meas:
+            
+            self._smu.write(f'smub.source.levelv = {i}')
+            t_din.append(time.time() - start_time)
+            v_din, i_din = self._smu.query('print(smub.measure.iv())').split('\t')
+            volt_din.append(float(v.strip('\n')))
+            curr_din.append(float(i.strip('\n')))
+            
+            time.sleep(pw)
+            
+            self._smu.write(f'smub.source.levelv = {hslV}')
+            t_rem.append(time.time() - start_time)
+            v_rem, i_rem = self._smu.query('print(smub.measure.iv())').split('\t')
+            volt_rem.append(float(v.strip('\n')))
+            curr_rem.append(float(i.strip('\n')))
+            
+            time.sleep(pw)
+        
+        self._smu.write('smub.source.output = smub.OUTPUT_OFF')
+        
+        return np.array(t_din), np.array(volt_din), np.array(curr_din), np.array(t_rem), np.array(volt_rem), np.array(curr_rem)
+        
     def stress_DC(self, V, time_interval, total_time):
         # Configuración del SMU
         #self._smu.write('display.measure.func = display.MEASURE_DCAMPS')  # Configurar el modo de medición a corriente
