fb6e0ee73eac5eeb93cc8a466c96f31bf14a52a5
Javi Tau <javitau459@gmail.com>
2023-08-26 00:46:48 -0300

    Cambio el save_csv para que actualice el numero automaticamente

--- save_csv.py
+++ save_csv.py
@@ -3,11 +3,18 @@ import matplotlib.pyplot as plt
 import os
 
 def save_csv(*data, filename, root='.\\', delimiter=',', header='', rewrite=False):
+    
     isfile = os.path.isfile(root+filename+'.csv')
-    if not rewrite and isfile:
-        print('Este archivo ya existe, para sobreescribirlo usar el argumento rewrite')
-        return
+    num = 0
+    while not rewrite and isfile:
+        num += 1
+        isfile = os.path.isfile(root+filename+f'-{num}'+'.csv')
+        
     if isfile:
         print('ATENCIÓN: SE SOBREESCRIBIRÁ EL ARCHIVO')
-    np.savetxt(root+filename+'.csv', np.transpose(np.array([*data])), header=header, delimiter=delimiter)
+    
+    if num == 0:
+        np.savetxt(root+filename+'.csv', np.transpose(np.array([*data])), header=header, delimiter=delimiter)
+    else:
+        np.savetxt(root+filename+f'-{num}'+'.csv', np.transpose(np.array([*data])), header=header, delimiter=delimiter)
     return
